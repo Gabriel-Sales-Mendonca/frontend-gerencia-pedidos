3975f3fa7bccc8dc104576101247740ef899e691
Gabriel Sales <gabrielsales07112018@gmail.com>
2025-05-17 13:42:51 -0300

    refactor: Mudei o layout

--- src/app/(private)/(orders)/page.tsx
+++ src/app/(private)/(orders)/page.tsx
@@ -2,6 +2,7 @@
 
 import { useOrders } from "@/hooks/useOrders"
 import { formatDate } from '@/utils/formatDate'
+import { useState } from "react"
 
 export default function Home() {
   const {
@@ -49,7 +50,7 @@ export default function Home() {
 
               {expandedOrders[key] && (
                 <ul className="ml-4 mt-2 space-y-2 border-l border-gray-300 pl-4">
-                  <li className="grid grid-cols-5 font-medium text-gray-500">
+                  <li className="grid grid-cols-[1fr_1fr_1fr_1fr_1fr] font-medium text-gray-500">
                     <span>Produto</span>
                     <span>Destino</span>
                     <span>Localização</span>
@@ -57,75 +58,83 @@ export default function Home() {
                     <span>Fim</span>
                   </li>
                   {orderDetails[key]?.map((detail) => (
-                    <li
-                      key={detail.id}
-                      className="grid grid-cols-5 items-center text-sm bg-gray-50 p-2 rounded-md gap-2"
-                    >
-                      <span>{detail.product_id}</span>
-                      <span>
-                        {editingDestinationId === detail.id ? (
-                          <div className="flex items-center gap-2">
-                            <select
-                              value={destinationUpdates[detail.id] || detail.destination_id || ''}
-                              onChange={(e) =>
-                                handleDestinationChange(detail.id, parseInt(e.target.value))
-                              }
-                              className="border border-gray-300 rounded p-1 text-sm cursor-pointer"
+                    <div key={detail.id} className="mb-6 rounded border border-gray-300">
+                      <li className="grid grid-cols-[1fr_1fr_1fr_1fr_1fr] items-center text-sm bg-white p-2 rounded-t gap-2">
+                        <span>{detail.product_id}</span>
+                        <span>
+                          {editingDestinationId === detail.id ? (
+                            <div className="flex items-center gap-2">
+                              <select
+                                value={destinationUpdates[detail.id] || detail.destination_id || ''}
+                                onChange={(e) =>
+                                  handleDestinationChange(detail.id, parseInt(e.target.value))
+                                }
+                                className="border border-gray-300 rounded p-1 text-sm cursor-pointer"
+                              >
+                                <option value="">Selecione</option>
+                                {locations.map((loc) => (
+                                  <option key={loc.id} value={loc.id} className="cursor-pointer">
+                                    {loc.name}
+                                  </option>
+                                ))}
+                              </select>
+                              <button
+                                onClick={() => {
+                                  setEditingDestinationId(null)
+                                  setDestinationUpdates((prev) => {
+                                    const updated = { ...prev }
+                                    delete updated[detail.id]
+                                    return updated
+                                  })
+                                }}
+                                className="cursor-pointer"
+                              >
+                                <span className="material-symbols-outlined text-red-600 rounded-full hover:bg-gray-200">
+                                  close
+                                </span>
+                              </button>
+                              <button
+                                onClick={() =>
+                                  updateDestination(detail.id, order.order_id, order.company_id)
+                                }
+                                className="text-blue-600 text-xs hover:underline cursor-pointer"
+                              >
+                                <span className="material-symbols-outlined text-green-600 rounded-full hover:bg-gray-200">
+                                  check
+                                </span>
+                              </button>
+                            </div>
+                          ) : (
+                            <span
+                              onClick={() => setEditingDestinationId(detail.id)}
+                              className="text-blue-600 cursor-pointer hover:underline"
                             >
-                              <option value="">Selecione</option>
-                              {locations.map((loc) => (
-                                <option key={loc.id} value={loc.id} className='cursor-pointer'>
-                                  {loc.name}
-                                </option>
-                              ))}
-                            </select>
-                            <button onClick={() => {
-                              setEditingDestinationId(null)
-                              setDestinationUpdates(prev => {
-                                const updated = { ...prev };
-                                delete updated[detail.id]; // <- Limpa o valor salvo
-                                return updated;
-                              });
-                            }}
-                              className='cursor-pointer'
-                            >
-                              <span className="material-symbols-outlined text-red-600 rounded-full hover:bg-gray-200">close</span>
-                            </button>
-                            <button
-                              onClick={() =>
-                                updateDestination(detail.id, order.order_id, order.company_id)
-                              }
-                              className="text-blue-600 text-xs hover:underline cursor-pointer"
-                            >
-                              <span className="material-symbols-outlined text-green-600 rounded-full hover:bg-gray-200">check</span>
-                            </button>
-                          </div>
-                        ) : (
-                          <span
-                            onClick={() => setEditingDestinationId(detail.id)}
-                            className="text-blue-600 cursor-pointer hover:underline"
+                              {detail.destinationLocation?.name ?? 'Nenhum'}
+                            </span>
+                          )}
+                        </span>
+                        <span>
+                          {detail.location?.name ?? '—'}
+                          <button
+                            onClick={() =>
+                              updateLocation(detail.id, order.order_id, order.company_id)
+                            }
+                            className="ml-3 bg-blue-500 text-white text-xs px-2 py-1 rounded hover:bg-blue-600 transition-colors cursor-pointer"
                           >
-                            {detail.destinationLocation?.name ?? 'sem destino'}
-                          </span>
-                        )}
-                      </span>
-                      <span>{detail.location?.name ?? '—'}
-                        <button
-                          onClick={() =>
-                            updateLocation(detail.id, order.order_id, order.company_id)
-                          }
-                          className="ml-3 bg-blue-500 text-white text-xs px-2 py-1 rounded hover:bg-blue-600 transition-colors cursor-pointer"
-                        >
-                          Receber
-                        </button>
-                      </span>
-                      <span>
-                          — / — / —
-                      </span>
-                      <span>
+                            Receber
+                          </button>
+                        </span>
+                        <span>— / — / —</span>
+                        <span className="flex items-center">
                           {formatDate(detail.location_delivery_date)}
-                      </span>
-                    </li>
+                          <button className="ml-3 text-center flex">
+                            <span className="material-symbols-outlined hover:text-orange-600 cursor-pointer">
+                              edit_square
+                            </span>
+                          </button>
+                        </span>
+                      </li>
+                    </div>
                   ))}
                 </ul>
               )}
